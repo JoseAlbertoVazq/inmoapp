2e897b9c8c3cc198723eb6c9da9b5bc875208b09
JoseAlbertoVazq <danielguillen10>
2019-02-23 13:39:44 +0100

    category response

--- inmoapp/app/src/main/java/dam/javazquez/inmoapp/responses/CategoryResponse.java
+++ inmoapp/app/src/main/java/dam/javazquez/inmoapp/responses/CategoryResponse.java
@@ -1,4 +1,22 @@
 package dam.javazquez.inmoapp.responses;
 
 public class CategoryResponse {
+
+    private String name;
+
+    public CategoryResponse() {
+
+    }
+
+    public CategoryResponse(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
